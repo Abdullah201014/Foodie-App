7668763d77676022097b8a9f9e795c2ebed57684
Abdullah Alkaff <abdullahalkaff@Abdullahs-MacBook-Pro.local>
2019-10-10 10:58:14 +0300

    Init repo

---
+++ lib/ui/Forget_page.dart
@@ -0,0 +1,171 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:the_gorgeous_login/style/theme.dart' as Theme;
+import 'package:the_gorgeous_login/utils/bubble_indication_painter.dart';
+
+import './login_page.dart';
+
+class ForgetPage extends StatefulWidget {
+  ForgetPage({Key key}) : super(key: key);
+
+  @override
+  _ForgetPage createState() => new _ForgetPage();
+}
+
+class _ForgetPage extends State<ForgetPage>
+with SingleTickerProviderStateMixin {
+
+  final FocusNode myFocusNodeEmailForget = FocusNode();
+
+  TextEditingController forgetEmailController = new TextEditingController();
+
+  bool _obscureTextForget = true;
+
+
+@override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+       appBar: AppBar(
+              title: Text("Main Page"),
+            ),
+      body: NotificationListener<OverscrollIndicatorNotification>(
+        onNotification: (overscroll) {
+          overscroll.disallowGlow();
+        },
+        child: SingleChildScrollView(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height >= 775.0
+                    ? MediaQuery.of(context).size.height
+                    : 775.0,
+                decoration: new BoxDecoration(
+                  gradient: new LinearGradient(
+                      colors: [
+                        Theme.Colors.loginGradientStart,
+                        Theme.Colors.loginGradientEnd
+                      ],
+                      begin: const FractionalOffset(0.0, 0.0),
+                      end: const FractionalOffset(1.0, 1.0),
+                      stops: [0.0, 1.0],
+                      tileMode: TileMode.clamp),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 75.0),
+                      child: new Image(
+                          width: 250.0,
+                          height: 191.0,
+                          fit: BoxFit.fill,
+                          image: new AssetImage('assets/img/login_logo.png')),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: _buildForget(context),
+                    ),
+                 ],
+                ),
+              ),
+            ),
+      ),
+    );
+  }
+
+
+  Widget _buildForget(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 23.0),
+      child: Column(
+        children: <Widget>[
+          Stack(
+            alignment: Alignment.topCenter,
+            overflow: Overflow.visible,
+            children: <Widget>[
+              Card(
+                elevation: 2.0,
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Container(
+                  width: 300.0,
+                  height: 130.0,
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(
+                            top: 20.0, bottom: 20.0, left: 25.0, right: 25.0),
+                        child: TextField(
+                          focusNode: myFocusNodeEmailForget,
+                          controller: forgetEmailController,
+                          keyboardType: TextInputType.text,
+                          textCapitalization: TextCapitalization.words,
+                          style: TextStyle(
+                              fontFamily: "WorkSansSemiBold",
+                              fontSize: 16.0,
+                              color: Colors.black),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            icon: Icon(
+                              FontAwesomeIcons.envelope,
+                              color: Colors.blue,
+                            ),
+                            hintText: "Email",
+                            hintStyle: TextStyle(
+                                fontFamily: "WorkSansSemiBold", fontSize: 16.0),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 250.0,
+                        height: 1.0,
+                        color: Colors.grey[400],
+                      ),
+
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 120.0),
+                decoration: new BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
+
+                  gradient: new LinearGradient(
+                      colors: [
+                        Colors.blue,
+                        Colors.blue,
+                      ],
+                      begin: const FractionalOffset(0.2, 0.2),
+                      end: const FractionalOffset(1.0, 1.0),
+                      stops: [0.0, 1.0],
+                      tileMode: TileMode.clamp),
+                ),
+                child: MaterialButton(
+                    highlightColor: Colors.transparent,
+                    splashColor: Theme.Colors.loginGradientEnd,
+                    //shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5.0))),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 42.0),
+                      child: Text(
+                        "Forget",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25.0,
+                            fontFamily: "WorkSansBold"),
+                      ),
+                    ), onPressed: () =>
+                        Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => LoginPage()))),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+}
---
+++ lib/ui/Main_page.dart
@@ -0,0 +1,67 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:the_gorgeous_login/style/theme.dart' as Theme;
+import 'package:the_gorgeous_login/utils/bubble_indication_painter.dart';
+
+
+
+class MainPage extends StatefulWidget {
+  MainPage({Key key}) : super(key: key);
+
+  @override
+  _MainPage createState() => new _MainPage();
+} 
+
+
+class _MainPage extends State<MainPage>
+with SingleTickerProviderStateMixin {
+@override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+       appBar: AppBar(
+              title: Text("Main Page"),
+            ),
+      body: NotificationListener<OverscrollIndicatorNotification>(
+        onNotification: (overscroll) {
+          overscroll.disallowGlow();
+        },
+        child: SingleChildScrollView(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height >= 775.0
+                    ? MediaQuery.of(context).size.height
+                    : 775.0,
+                decoration: new BoxDecoration(
+                  gradient: new LinearGradient(
+                      colors: [
+                        Theme.Colors.loginGradientStart,
+                        Theme.Colors.loginGradientEnd
+                      ],
+                      begin: const FractionalOffset(0.0, 0.0),
+                      end: const FractionalOffset(1.0, 1.0),
+                      stops: [0.0, 1.0],
+                      tileMode: TileMode.clamp),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 75.0),
+                      child: new Image(
+                          width: 250.0,
+                          height: 191.0,
+                          fit: BoxFit.fill,
+                          image: new AssetImage('assets/img/login_logo.png')),
+                    ),
+                   
+                   ],
+                ),
+              
+              ),
+            ),
+      ),
+    );
+  }
+}
